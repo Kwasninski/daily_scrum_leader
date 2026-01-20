2cba70c0cf3b03ee0b0c005a46b1662416dfcfe9
seba <sebastians9876@gmail.com>
2026-01-20 21:09:16 +0100

    fix app.run

--- app.py
+++ app.py
@@ -300,6 +300,6 @@ def api_change_flag_was_picked_for_team_member(id):
 
 
 
-# if __name__ == "__main__":
-#     app.run(port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
 
